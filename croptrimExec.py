9a714dd9369bffccc3dd5995cf63d69664999f54
Lingfeng Hou <lingfenghou@g.harvard.edu>
2018-11-26 15:58:07 -0500

    Ass

--- croptrimExec.py
+++ croptrimExec.py
@@ -1,14 +1,21 @@
 # -*- coding: utf-8 -*-
 """
-Load params generated in MATLAB and execute video cropping and trimming.
+Load params generated in MATLAB and execute video processing.
+	1) Crop to ROI
+	2) Trim so both videos start and end at the same time (or as close as possible)
+	3) Convert to grey image by setting saturation=0
 """
-
 from scipy.io import loadmat
-params = loadmat('/media/lingfeng/Data/Folder/params.mat')
+import os, subprocess
+
+params = loadmat('C:\Server\params2.mat')
 params = params['Params']
 for iDir in range(params.size):
-    for iVid in range(params[iDir]['Video'].size):
-        framenumber = params[iDir]['Video'][iVid]['FrameNumber'];
-        timestamp = params[iDir]['Video'][iVid]['Timestamp'];
-        crop = params[iDir]['Video'][iVid]['Crop'];
-        crop = params[iDir]['Video'][iVid]['File'];
+    for iVid in range(params[0, iDir]['Video'].size):
+        file 		= params[0, iDir]['Video'][0, iVid]['File'][0];
+        crop 		= params[0, iDir]['Video'][0, iVid]['Crop'][0];
+        frameNumber = params[0, iDir]['Video'][0, iVid]['FrameNumber'][0];
+        timestamp 	= params[0, iDir]['Video'][0, iVid]['Timestamp'][0];
+        command = 'ffmpeg -y -i "' + file + '.mp4"' + ' -vf "eq=saturation=0, crop=' + str(crop[2]) + ':' + str(crop[3]) + ':' + str(crop[0]) + ':' + str(crop[1]) + ', trim=start_frame=' + str(frameNumber[0]) + ':end_frame=' + str(frameNumber[-1] + 1) + '" "' + file + '_cropped.mp4"'
+        print(command)
+        subprocess.call(command, shell=True)
